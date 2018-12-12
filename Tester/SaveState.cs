aa3bc2eeb1a15273c5404d4b00a30f6faeea0800
Cedric Miller <cedric7m@yahoo.com>
2018-12-11 18:42:08 -0800

    sa

--- Tester/SaveState.cs
+++ Tester/SaveState.cs
@@ -16,20 +16,58 @@ namespace Tester
 
         public SaveState()
         {
+             //For loading data
+            
             fileName = "saveState.dat";
+            String savedData = "";
+
 
             //Open file
+
+            StreamReader readData = null;
+            try
+            {
+              readData = new StreamReader(fileName);
+              savedData   = readData.ReadLine();
+            }
+            catch (FileNotFoundException e)
+            {
+
+              throw new FileNotFoundException("File is not found");
+            }
+
+
+            loadedContent = JsonConvert.DeserializeObject<Data>(savedData); // load the contents here
+
             
-            loadedContent = null; // load the contents here
+
         }
 
         public SaveState(string fileName)
         {
             this.fileName = fileName;
 
-            //open file
+            String savedData = "";
 
-            loadedContent = null; // load the contents here
+
+            //Open file
+
+            StreamReader readData = null;
+            try
+            {
+                readData = new StreamReader(fileName);
+                savedData = readData.ReadLine();
+            }
+            catch (FileNotFoundException e)
+            {
+
+                throw new FileNotFoundException("File is not found");
+            }
+
+
+            loadedContent = JsonConvert.DeserializeObject<Data>(savedData); // load the contents here
+
+
         }
 
         //Retruns true if data saved successfully, false otherwise
@@ -38,6 +76,39 @@ namespace Tester
             Data toSave = new Data((LinkedList<User>)user, (LinkedList<Admin>)admin);
 
             //Save toSave in file
+
+            //Console.WriteLine(list.Contains(test1));
+            //Console.Read();
+
+            var saveState = JsonConvert.SerializeObject(toSave);
+
+
+            //For loading data
+            //LinkedList<User> deserializedProduct = JsonConvert.DeserializeObject<LinkedList<User>>(saveState);
+
+            //Console.Write(deserializedProduct);
+            //Console.Read();
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+                using (var tw = new StreamWriter(fileName, false))
+                {
+                    tw.WriteLine(saveState);
+
+                    tw.Close();
+                }
+
+            }
+
+            else if (!(File.Exists(fileName)))
+            {
+                using (var tw = new StreamWriter(fileName, false))
+                {
+                    tw.WriteLine(saveState);
+                    tw.Close();
+                }
+            }
 
             return true;
         }
